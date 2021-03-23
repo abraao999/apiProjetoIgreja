4a23af8f5ff4fb257dc9306b7e85ffab6fa2b368
abraao <abraaocpm@hotmail.com>
2021-03-22 21:19:18 -0300

    cria model de membros

--- dist/models/Membro.js
+++ dist/models/Membro.js
@@ -1,4 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize'); var _sequelize2 = _interopRequireDefault(_sequelize);
+var _bcryptjs = require('bcryptjs'); var _bcryptjs2 = _interopRequireDefault(_bcryptjs);
 
  class Membro extends _sequelize.Model {
   static init(sequelize) {
@@ -24,29 +25,104 @@
             },
           },
         },
-        descricao: {
+        telefone: {
           type: _sequelize2.default.STRING,
           defaultValue: "",
           validate: {
             len: {
-              args: [3, 50],
-              msg: "Campo descrição deve ter entre 3 e 50 caracteres",
+              args: [3, 20],
+              msg: "Campo descrição deve ter entre 3 e 20 caracteres",
             },
           },
         },
-        d: {
+        estado_civil: {
           type: _sequelize2.default.STRING,
           defaultValue: "",
           validate: {
             len: {
               args: [3, 50],
-              msg: "Campo descrição deve ter entre 3 e 50 caracteres",
+              msg: "Campo estado civil deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        data_batismo: {
+          type: _sequelize2.default.DATE,
+          defaultValue: "",
+          validate: {
+            isDate: {
+              msg: "Campo estado civíl deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        profissao: {
+          type: _sequelize2.default.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 50],
+              msg: "Campo profissão deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        email: {
+          type: _sequelize2.default.STRING,
+          defaultValue: "",
+          unique: { msg: "E-mail existente" },
+          validate: {
+            isEmail: {
+              msg: "E-mail inválido",
+            },
+          },
+        },
+        password_hash: {
+          type: _sequelize2.default.STRING,
+          defaultValue: "",
+        },
+        password: {
+          type: _sequelize2.default.VIRTUAL,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [5, 50],
+              msg: "Campo senha deve ter entre 6 e 50 caracteres",
+            },
+          },
+        },
+        cargo_id: {
+          type: _sequelize2.default.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo cargo deve ser um inteiro",
+            },
+          },
+        },
+        function_id: {
+          type: _sequelize2.default.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo função deve ser um inteiro",
+            },
+          },
+        },
+        setor_id: {
+          type: _sequelize2.default.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo setor deve ser um inteiro",
             },
           },
         },
       },
       { sequelize }
     );
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await _bcryptjs2.default.hash(user.password, 8);
+      }
+    });
     return this;
   }
 
--- src/models/Membro.js
+++ src/models/Membro.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from "sequelize";
+import bcryptjs from "bcryptjs";
 
 export default class Membro extends Model {
   static init(sequelize) {
@@ -24,29 +25,104 @@ export default class Membro extends Model {
             },
           },
         },
-        descricao: {
+        telefone: {
           type: Sequelize.STRING,
           defaultValue: "",
           validate: {
             len: {
-              args: [3, 50],
-              msg: "Campo descrição deve ter entre 3 e 50 caracteres",
+              args: [3, 20],
+              msg: "Campo descrição deve ter entre 3 e 20 caracteres",
             },
           },
         },
-        d: {
+        estado_civil: {
           type: Sequelize.STRING,
           defaultValue: "",
           validate: {
             len: {
               args: [3, 50],
-              msg: "Campo descrição deve ter entre 3 e 50 caracteres",
+              msg: "Campo estado civil deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        data_batismo: {
+          type: Sequelize.DATE,
+          defaultValue: "",
+          validate: {
+            isDate: {
+              msg: "Campo estado civíl deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        profissao: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 50],
+              msg: "Campo profissão deve ter entre 3 e 50 caracteres",
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          unique: { msg: "E-mail existente" },
+          validate: {
+            isEmail: {
+              msg: "E-mail inválido",
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [5, 50],
+              msg: "Campo senha deve ter entre 6 e 50 caracteres",
+            },
+          },
+        },
+        cargo_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo cargo deve ser um inteiro",
+            },
+          },
+        },
+        function_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo função deve ser um inteiro",
+            },
+          },
+        },
+        setor_id: {
+          type: Sequelize.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Campo setor deve ser um inteiro",
             },
           },
         },
       },
       { sequelize }
     );
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
     return this;
   }
 
