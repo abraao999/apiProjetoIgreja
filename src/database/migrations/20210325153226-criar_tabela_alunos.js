01bc8f2006d00754e79d7ab2d52a721ede5cbe45
unknown <abraaocpm@hotmail.com>
2021-03-25 14:06:06 -0300

    edir migration aluno

--- src/database/migrations/20210325153226-criar_tabela_alunos.js
+++ src/database/migrations/20210325153226-criar_tabela_alunos.js
@@ -1,21 +1,29 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable("alunos", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      membro_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: "membros", key: "id" },
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: (queryInterface, Sequelize) => queryInterface.dropTable("alunos"),
 };
+
