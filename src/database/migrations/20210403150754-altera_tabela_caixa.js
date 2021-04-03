7176cb49d932d825986d4c49062fae61db73739b
abraao <abraaocpm@hotmail.com>
2021-04-03 12:14:04 -0300

    add colum setor_id na tabela caixa

---
+++ src/database/migrations/20210403150754-altera_tabela_caixa.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.addColumn("caixas", "setor_id", {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: { model: "setors", key: "id" },
+      onDelete: "SET NULL",
+      onUpdate: "CASCADE",
+    }),
+
+  down: () => {},
+};
