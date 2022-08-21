57b9b7f46fe9c867cd7284dc196b42feeca6b6f9
abraao <abraaocpm@hotmail.com>
2022-08-21 13:06:48 -0300

    das

--- dist/controllers/Livraria/LivrariaVendaItenController.js
+++ dist/controllers/Livraria/LivrariaVendaItenController.js
@@ -18,18 +18,13 @@ class LivrariaVendaItenController {
   }
 
   async index(req, res) {
-    // const dados = await knex("livraria_venda_itens")
-    //   .join("livraria_livros", "livro_id", "=", "livraria_livros.id")
-    //   .select(
-    //     "livraria_venda_itens.*",
-    //     "livraria_livros.descricao as descricao"
-    //   );
-    const dados = await _knexfile2.default.call(void 0, "livraria_venda_itens");
-    // .join("livraria_livros", "livro_id", "=", "livraria_livros.id")
-    // .select(
-    //   "livraria_venda_itens.*",
-    //   "livraria_livros.descricao as descricao"
-    // );
+    const dados = await _knexfile2.default.call(void 0, "livraria_venda_itens")
+      .join("livraria_livros", "livro_id", "=", "livraria_livros.id")
+      .select(
+        "livraria_venda_itens.*",
+        "livraria_livros.descricao as descricao"
+      );
+
     res.json(dados);
   }
 
